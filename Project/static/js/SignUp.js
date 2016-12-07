6eea14ed2b8290d6b54ead0ebe013bfdc16ebe78
narendra <narendra@MacBook-Pro.local>
2016-12-07 11:36:16 -0800

    js and html folder

---
+++ Project/static/js/SignUp.js
@@ -0,0 +1,29 @@
+var login=angular.module('login',['ui.router'])
+.controller('loginctrl',function ($scope,$location) {
+	// alert("hello");
+
+	$scope.sign=function(){
+		$location.path('/sign');
+	}
+
+	$scope.login=function(){
+		$location.path('/login');
+	}
+})
+
+.config(function($stateProvider,$urlRouterProvider){
+	$urlRouterProvider.otherwise('/sign');
+	$stateProvider												//Default-page
+	.state('sign',{
+		url:"/sign",
+		templateUrl:'sign.html',
+		// controller:'homepage'
+	})
+
+	.state('login',{									//Second-page
+		url:"/login",
+		templateUrl:'login.html',
+		// controller:'orderpage'
+	})
+
+})	
