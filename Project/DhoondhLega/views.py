5b95bb137f97857edd18b81b07ccdd68c1ee4540
dhrumil44 <dhrumildesai2014ms@yahoo.com>
2016-12-07 17:47:19 -0800

    check example to get data from json

--- Project/DhoondhLega/views.py
+++ Project/DhoondhLega/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import urllib, json
 from datetime import datetime, timedelta
+from json2html import *
 
 
 def index(request):
@@ -14,4 +15,4 @@ def theater_movie(request):
 	url = "https://api.themoviedb.org/3/discover/movie?api_key=8419dce727cd16afcc50b6e085faf444&primary_release_date.gte=2016-11-15&primary_release_date.lte="+str(current_dt)
 	response = urllib.urlopen(url)
 	data = json.loads(response.read())
-	return render(request, "theater_movie.html" , {"data":data})
+	return render(request, "theater_movie.html" , {"data":data['results']})
