a2c1c278154a8898e7603f6ae0d6d18c83faac1b
dhrumil44 <dhrumildesai2014ms@yahoo.com>
2016-12-12 13:37:42 -0800

    view and templet created

--- Project/DhoondhLega/urls.py
+++ Project/DhoondhLega/urls.py
@@ -4,8 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^movie/$', views.theater_movies, name='theater_movies'),
-    url(r'^movie/popular/$', views.popular_movies, name='popular_movies'),
-    url(r'^movie/top/$', views.top_rated_movies, name='top_rated_movies'),
-    url(r'^movie/upcoming/$', views.upcoming_movies, name='upcoming_movies'),
+    url(r'^house/post/$', views.house_post, name='house_post'),
+
+    # url(r'^movie/$', views.theater_movies, name='theater_movies'),
+    # url(r'^movie/popular/$', views.popular_movies, name='popular_movies'),
+    # url(r'^movie/top/$', views.top_rated_movies, name='top_rated_movies'),
+    # url(r'^movie/upcoming/$', views.upcoming_movies, name='upcoming_movies'),
 ]
--- Project/DhoondhLega/views.py
+++ Project/DhoondhLega/views.py
@@ -3,41 +3,55 @@ from django.http import HttpResponse
 import urllib, json
 from datetime import datetime, timedelta
 from json2html import *
+from .forms import HouseForm
 
 
 def index(request):
     return render(request, "index.html")
 
-
-def theater_movies(request):
-	current_dt = datetime.today().strftime("%Y-%m-%d")
-	previous_dt = (datetime.now() - timedelta(days=40)).strftime("%Y-%m-%d")
-	url = "https://api.themoviedb.org/3/discover/movie?api_key=8419dce727cd16afcc50b6e085faf444&include_video=true&primary_release_date.gte="+str(previous_dt)+"&primary_release_date.lte="+str(current_dt)
-	response = urllib.urlopen(url)
-	data = json.loads(response.read())
-	return render(request, "theater_movie.html" , {"data":data['results']})
-
-
-def popular_movies(request):
-	url = "https://api.themoviedb.org/3/discover/movie?api_key=8419dce727cd16afcc50b6e085faf444&sort_by=popularity.desc"
-	response = urllib.urlopen(url)
-	data = json.loads(response.read())
-	return render(request, "theater_movie.html" , {"data":data['results']})
+def house_post(request):
+	form = HouseForm(request.POST or None , request.FILES or None )
+	# if Request.method == "POST":
+	# 	print Request.POST.get("Content")
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		
+		return render(request, "post_house.html")
 
 
-def top_rated_movies(request):
-	url = "https://api.themoviedb.org/3/discover/movie?api_key=8419dce727cd16afcc50b6e085faf444&sort_by=vote_average.desc"
-	response = urllib.urlopen(url)
-	data = json.loads(response.read())
-	return render(request, "theater_movie.html" , {"data":data['results']})
+	else:
 
-def upcoming_movies(request):
-	current_dt = datetime.today().strftime("%Y-%m-%d")
-	next_dt = (datetime.now() + timedelta(days=40)).strftime("%Y-%m-%d")
-	url = "https://api.themoviedb.org/3/discover/movie?api_key=8419dce727cd16afcc50b6e085faf444&include_video=true&primary_release_date.gte="+str(current_dt)+"&primary_release_date.lte="+str(next_dt)
-	response = urllib.urlopen(url)
-	data = json.loads(response.read())
-	return render(request, "theater_movie.html" , {"data":data['results']})
+		return render(request, "post_house.html",{"form":form})
+# def theater_movies(request):
+# 	current_dt = datetime.today().strftime("%Y-%m-%d")
+# 	previous_dt = (datetime.now() - timedelta(days=40)).strftime("%Y-%m-%d")
+# 	url = "https://api.themoviedb.org/3/discover/movie?api_key=8419dce727cd16afcc50b6e085faf444&include_video=true&primary_release_date.gte="+str(previous_dt)+"&primary_release_date.lte="+str(current_dt)
+# 	response = urllib.urlopen(url)
+# 	data = json.loads(response.read())
+# 	return render(request, "theater_movie.html" , {"data":data['results']})
+
+
+# def popular_movies(request):
+# 	url = "https://api.themoviedb.org/3/discover/movie?api_key=8419dce727cd16afcc50b6e085faf444&sort_by=popularity.desc"
+# 	response = urllib.urlopen(url)
+# 	data = json.loads(response.read())
+# 	return render(request, "theater_movie.html" , {"data":data['results']})
+
+
+# def top_rated_movies(request):
+# 	url = "https://api.themoviedb.org/3/discover/movie?api_key=8419dce727cd16afcc50b6e085faf444&sort_by=vote_average.desc"
+# 	response = urllib.urlopen(url)
+# 	data = json.loads(response.read())
+# 	return render(request, "theater_movie.html" , {"data":data['results']})
+
+# def upcoming_movies(request):
+# 	current_dt = datetime.today().strftime("%Y-%m-%d")
+# 	next_dt = (datetime.now() + timedelta(days=40)).strftime("%Y-%m-%d")
+# 	url = "https://api.themoviedb.org/3/discover/movie?api_key=8419dce727cd16afcc50b6e085faf444&include_video=true&primary_release_date.gte="+str(current_dt)+"&primary_release_date.lte="+str(next_dt)
+# 	response = urllib.urlopen(url)
+# 	data = json.loads(response.read())
+# 	return render(request, "theater_movie.html" , {"data":data['results']})
 
 # For trailers
 #  https://api.themoviedb.org/3/movie/id/videos?api_key=8419dce727cd16afcc50b6e085faf444&language=en-US
