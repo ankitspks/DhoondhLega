d50f98b6cc4170974be4374ead09a38e6f0e1ece
Govinda <ramroketa101@gmail.com>
2016-12-12 14:21:05 -0800

    create movie models

--- Project/DhoondhLega/models.py
+++ Project/DhoondhLega/models.py
@@ -1,5 +1,14 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+	name = models.CharField(max_length=20)
+	genre = models.CharField(max_length=25)
+	language = models.CharField(max_length=50)
+	cast = models.CharField(max_length=50)
+	image = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no-img.jpg')
+	reviews = models.CharField(max_length=1000)
+	rating = models.IntegerField(default=1)
+	
+	def __str__(self):
+		return self.name
