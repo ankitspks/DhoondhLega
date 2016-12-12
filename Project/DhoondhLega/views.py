a090c0f925d27e7bf671d8c504a435a5588b5d94
Aman Sandhu <Aman Sandhu>
2016-12-12 12:28:54 -0800

    model created

--- Project/DhoondhLega/views.py
+++ Project/DhoondhLega/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
 import urllib, json
 from datetime import datetime, timedelta
 
@@ -11,7 +11,9 @@ def index(request):
 def theater_movie(request):
 	current_dt = datetime.today().strftime("%Y-%m-%d")
 	previous_dt = (datetime.now() - timedelta(days=20)).strftime("%Y-%m-%d")
-	url = "https://api.themoviedb.org/3/discover/movie?api_key=8419dce727cd16afcc50b6e085faf444&primary_release_date.gte=2016-11-15&primary_release_date.lte="+str(current_dt)
+	url = "https://api.themoviedb.org/3/discover/movie?api_key=45b04bcb134812d86b3823d97ba87bd6"	
+	# url = "https://api.themoviedb.org/3/discover/movie?api_key=8419dce727cd16afcc50b6e085faf444&primary_release_date.gte=2016-11-15&primary_release_date.lte="+str(current_dt)
 	response = urllib.urlopen(url)
 	data = json.loads(response.read())
-	return render(request, "theater_movie.html" , {"data":data})
+	# print data
+	return JsonResponse(data);
